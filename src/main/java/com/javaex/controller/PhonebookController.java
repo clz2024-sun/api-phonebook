7091edd806fe6e1e7fb64a9185b9c2864431f60b
clz2024.sun <clz2024.sun@gmail.com>
2024-09-23 13:25:26 +0900

    2024-09-23

--- src/main/java/com/javaex/controller/PhonebookController.java
+++ src/main/java/com/javaex/controller/PhonebookController.java
@@ -3,24 +3,26 @@ package com.javaex.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.javaex.service.PhonebookService;
+import com.javaex.util.JsonResult;
 import com.javaex.vo.PersonVo;
 
-@Controller
+@RestController
 public class PhonebookController {
 
 	@Autowired
 	private PhonebookService phonebookService;
 	
-	@ResponseBody
-	@RequestMapping(value="/api/persons", method=RequestMethod.GET)
+	
+	@GetMapping("/api/persons")
 	public List<PersonVo> getList() {
 		System.out.println("PhonebookController.getList()");
 		
@@ -29,8 +31,8 @@ public class PhonebookController {
 		return personList;
 	}
 	
-	@ResponseBody
-	@RequestMapping(value="/api/persons", method=RequestMethod.POST)
+	
+	@PostMapping("/api/persons")
 	public int addPerson(@RequestBody PersonVo personVo) {
 		System.out.println("PhonebookController.addPerson()");
 		
@@ -39,14 +41,46 @@ public class PhonebookController {
 		return count;
 	}
 	
-	@ResponseBody
-	@RequestMapping(value="/api/persons/{no}", method=RequestMethod.DELETE)
-	public int delPerson(@PathVariable(value="no") int no) {
+	
+	@DeleteMapping("/api/persons/{no}")
+	public JsonResult delPerson(@PathVariable(value="no") int no) {
 		System.out.println("PhonebookController.addPerson()");
 		System.out.println(no);
 		
 		int count = phonebookService.exePersonDelete(no);
-		return count;
+		
+		if(count != 1) { //삭제안됨
+			return JsonResult.fail("해당번호가 없습니다.");
+		}else { //삭제됨
+			return JsonResult.success(count);
+		}
+		
 	}
+	
+	@GetMapping("/api/persons/{no}")
+	public JsonResult getPerson(@PathVariable(value="no") int personId) {
+		System.out.println("PhonebookController.getPerson()");
+		
+		PersonVo personVo = phonebookService.exeEditForm(personId);
+		
+		if(personVo == null) {
+			return JsonResult.fail("번호가 없습니다.");
+		
+		}else {
+			return JsonResult.success(personVo);
+		}
+		
+	}
+	
+	
+	@PutMapping("/api/persons/{no}")
+	public JsonResult updatePerson() {
+		System.out.println("PhonebookController.updatePerson()");
+		
+		return null;
+	}
+	
+	
+	
 	
 }
